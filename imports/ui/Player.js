c4648b18d391f16b28d907f3ba691267405a1298
spixooze <s_simon_s@hotmail.com>
2017-03-25 14:47:32 +0100

    Add more styles

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -7,12 +7,20 @@ export default class Player extends React.Component {
 		let player = this.props.player;
 		return (
 			<div key={player._id} className="item">
-				<p>
-					{player.name} has {player.score} point(s).
-				</p>
-				<button className="button button--round" onClick={() => Players.update(player._id, {$inc: {score:-1}})}>-1</button>
-				<button className="button button--round" onClick={() => Players.update(player._id, {$inc: {score: 1}})}>+1</button>
-				<button className="button button--round" onClick={() => Players.remove(player._id)}>X</button>
+				<div className="player">
+					<div>
+						<h3 className="player__name">{player.name}</h3>
+						<p className="player__stats">
+						{player.score} point(s).
+						</p>
+					</div>
+					<div className="player__actions">
+						<button className="button button--round" onClick={() => Players.update(player._id, {$inc: {score:-1}})}>-1</button>
+						<button className="button button--round" onClick={() => Players.update(player._id, {$inc: {score: 1}})}>+1</button>
+						<button className="button button--round" onClick={() => Players.remove(player._id)}>X</button>
+					</div>
+					
+				</div>
 			</div>
 		);
 	}
